083d251327a842b1927dbd6944d82fc2dff92c41
BQTUFGAMING <BQTUFGAMING@192.168.1.33>
2025-12-24 11:08:37 +0530

    24-12-2025

---
+++ src/main/java/ex_06_Ternary_Operator/Task.java
@@ -0,0 +1,24 @@
+package ex_06_Ternary_Operator;
+
+public class Task {
+	
+	// Ternary Operator (No if-else allowed)
+    // Problem
+    //
+    //Write a Statement that returns:
+    //  int marks = 100;
+    //"PASS" if marks ≥ 40
+    //
+    //"FAIL" otherwise
+
+	public static void main(String[] args) {
+		
+		int marks = 30;
+        String result  = (marks >=40) ? "PASS" : "FAIL";
+        System.out.println(result);
+        
+        
+
+	}
+
+}
